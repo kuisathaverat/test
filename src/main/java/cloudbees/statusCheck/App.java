7269e484455046f78f2c4771050389c62138b967
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2016-05-16 12:14:38 +0200

    Add files via upload

---
+++ src/main/java/cloudbees/statusCheck/App.java
@@ -0,0 +1,13 @@
+package cloudbees.statusCheck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
