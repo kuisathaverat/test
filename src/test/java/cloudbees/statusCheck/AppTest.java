b8fdb37a3bccb17bee95ecfc02609c1c82ccc803
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2017-07-20 20:51:31 +0200

    Update AppTest.java

--- src/test/java/cloudbees/statusCheck/AppTest.java
+++ src/test/java/cloudbees/statusCheck/AppTest.java
@@ -33,7 +33,7 @@ public class AppTest
      */
     public void testApp()
     {
-    	//throw new RuntimeException(" KO - 8");
+    	//throw new RuntimeException(" KO - 9");
         assertTrue( true );
     }
 }
